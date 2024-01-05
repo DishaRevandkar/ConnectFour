fcfb7286de7e696de626e5445127b03f03673803
Disha Revandkar <55402567+DishaRevandkar@users.noreply.github.com>
2024-01-05 05:57:06 -0500

    Update ConnectFourGUI.java

--- src/connectfour/gui/ConnectFourGUI.java
+++ src/connectfour/gui/ConnectFourGUI.java
@@ -18,7 +18,7 @@ import javafx.stage.Stage;
 /**
  * The GUI for Connect Four game. This class encapsulates the View and Controller portions of the MVC architecture.
  *
- * @author  Disha Revandkar  dr6742@rit.edu
+ * @author  Disha Revandkar 
  */
 public class ConnectFourGUI extends Application implements Observer {
 
